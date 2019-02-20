7e68221df7d401d032dd10131265f24b09430869
vshlg <varyasheleg14@gmail.com>
2019-02-21 00:07:08 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(str, bracketsConfig)  {
+    var list=[]
+    var config ={}
+    var j=1
+    for (var i=0;i<bracketsConfig.length;i++) {
+        config[bracketsConfig[i][j]] = bracketsConfig[i][0]
+    }
+    
+    for (var k=0;k<str.length;k++) {
+        if (list.length ===0) {
+            list.push(str[k])
+        }
+        else if (list[list.length-1]===config[str[k]] ) {
+            let elem = list.pop()       
+        }
+        else {
+            list.push(str[k])
+        }
+    }
+    if (list.length==0) {
+        return true
+    }
+    else {
+        return false
+    }
+    
+};
